341659b64d3e8899907f2afbbf4fb5b16bfd3a6b
Shantur Rathore <rathore4u@gmail.com>
2013-09-15 03:15:09 +0100

    Script to update forks and local manifest for CarbonRom

---
+++ update-forks.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+get_repo_path()
+{
+	
+        R=$1
+	R2=$(echo $R|sed s@\/@_@g)
+        echo android_$R2
+}
+
+do_update_fork()
+{
+	cd ../$1
+	REPO=$(get_repo_path $1)
+	git remote add $2 $3/$REPO
+	git fetch $2
+	git merge $2/$4
+	cd -
+}
+
+#update forked repos
+echo updating forked repos...
+TEAM_HYDRA_URL="https://github.com/Team-Hydra"
+BASE_BRANCH="cm-10.2"
+TEAM_HYDRA_NAME="TeamHydra"
+
+CM_URL="https://github.com/CyanogenMod"
+CM_NAME="cm"
+
+
+do_update_fork "device/htc/pyramid" $TEAM_HYDRA_NAME $TEAM_HYDRA_URL $BASE_BRANCH
+do_update_fork "device/htc/msm8660-common" $TEAM_HYDRA_NAME $TEAM_HYDRA_URL $BASE_BRANCH
+do_update_fork "hardware/qcom/gps-legacy" $TEAM_HYDRA_NAME $TEAM_HYDRA_URL $BASE_BRANCH
+do_update_fork "hardware/qcom/gps" $CM_NAME $CM_URL $BASE_BRANCH
+
+
+echo Update complete....
+
+
